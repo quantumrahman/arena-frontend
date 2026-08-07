87a45d6abeec4bd8904ca1c5c6ca4f54268467f1
rakibulrahmandev <rakibulrahman.webdev@gmail.com>
2026-08-07 12:35:43 +0600

    refactor: add background image and some overlays

--- src/app/(auth)/layout.js
+++ src/app/(auth)/layout.js
@@ -6,8 +6,10 @@ export const metadata = {
 
 export default function AuthLayout({ children }) {
     return (
-        <main className="flex min-h-screen w-full items-center justify-center bg-neutral-50">
-            {children}
+        <main className="relative min-h-screen overflow-hidden">
+            <div className="absolute inset-0 scale-105 bg-[url('/img/auth_bg.png')] bg-cover bg-center bg-no-repeat blur-[3px]"></div>
+            <div className="absolute inset-0 bg-black/30"></div>
+            <div className="relative z-10">{children}</div>
         </main>
     );
 }
